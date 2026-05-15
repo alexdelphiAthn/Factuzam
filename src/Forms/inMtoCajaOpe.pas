b03824258dd2219a116da04702324cf4b57cbe58
Alejandro Laorden <alejandro.laorden@proton.me>
2026-05-15 20:14:30 +0200

    Auto-update 2026-05-15 20:14:30

--- src/Forms/inMtoCajaOpe.pas
+++ src/Forms/inMtoCajaOpe.pas
@@ -1449,6 +1449,12 @@ begin
   end;
   if (AItem.Tag > 0) and (AEdit is TcxImageComboBox) then
   begin
+    // Dentro de cxGrid1DBTableView1EditKeyDown, justo despues del begin
+    // del if (AItem.Tag > 0) and (AEdit is TcxImageComboBox) then ...
+    ShowMessage('EditKeyDown llamado. Key=' + IntToStr(Key) +
+            ' Items=' + IntToStr(Combo.Properties.Items.Count) +
+            ' ItemIndex=' + IntToStr(Combo.ItemIndex) +
+            ' DroppedDown=' + BoolToStr(Combo.DroppedDown, True));
     Combo := TcxImageComboBox(AEdit);
     // Enter sobre celda vacia (y dropdown cerrado): abrimos el desplegable
     // para que el usuario pueda elegir. ForzarDespliegue via OnEnter no
