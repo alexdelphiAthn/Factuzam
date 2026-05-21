0d6933b3052da1765628afd1d4d7aa5e21e51549
Alejandro Laorden <alejandro.laorden@proton.me>
2026-05-21 16:50:29 +0200

    Auto-update 2026-05-21 16:50:29

--- fzam.dxSettings.pas
+++ fzam.dxSettings.pas
@@ -16,15 +16,26 @@
 
 unit fzam.dxSettings;
 
-//{$I cxVer.inc}
+{$I cxVer.inc}
 
 interface
 
 uses
-  Winapi.Windows, System.Classes, Vcl.Themes, dxCore, cxLookAndFeels,
-  dxSkinsCore, dxSkinsForm,
+  Winapi.Windows, System.Classes, Vcl.Themes, dxCore, cxLookAndFeels, dxSkinsCore, dxSkinsForm,
   dxSkinWXI, dxSkinTheBezier, dxSkinOffice2019Colorful, dxSkinOffice2019Black,
-  dxSkinOffice2019DarkGray, dxSkinOffice2019White, dxSkinBasic;
+  dxSkinOffice2019DarkGray, dxSkinOffice2019White, dxSkinBasic, dxSkinBlack, dxSkinBlue,
+  dxSkinBlueprint, dxSkinCaramel, dxSkinCoffee, dxSkinDarkroom, dxSkinDarkSide,
+  dxSkinDevExpressDarkStyle, dxSkinDevExpressStyle, dxSkinFoggy, dxSkinGlassOceans,
+  dxSkinHighContrast, dxSkiniMaginary, dxSkinLilian, dxSkinLiquidSky, dxSkinLondonLiquidSky,
+  dxSkinMcSkin, dxSkinMetropolis, dxSkinMetropolisDark, dxSkinMoneyTwins,
+  dxSkinOffice2007Black, dxSkinOffice2007Blue, dxSkinOffice2007Green, dxSkinOffice2007Pink,
+  dxSkinOffice2007Silver, dxSkinOffice2010Black, dxSkinOffice2010Blue, dxSkinOffice2010Silver,
+  dxSkinOffice2013DarkGray, dxSkinOffice2013LightGray, dxSkinOffice2013White,
+  dxSkinOffice2016Colorful, dxSkinOffice2016Dark, dxSkinPumpkin, dxSkinSeven,
+  dxSkinSevenClassic, dxSkinSharp, dxSkinSharpPlus, dxSkinSilver, dxSkinSpringtime,
+  dxSkinStardust, dxSkinSummer2008, dxSkinTheAsphaltWorld, dxSkinValentine,
+  dxSkinVisualStudio2013Blue, dxSkinVisualStudio2013Dark, dxSkinVisualStudio2013Light,
+  dxSkinVS2010, dxSkinWhiteprint, dxSkinXmas2008Blue;
 
 implementation
 
