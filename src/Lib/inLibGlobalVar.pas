2a4baf7ace47ce294c8acea4e2436d9db37f6e01
Alejandro Laorden <alejandro.laorden@proton.me>
2026-05-30 07:17:02 +0200

    Auto-update 2026-05-30 07:17:01

--- src/Lib/inLibGlobalVar.pas
+++ src/Lib/inLibGlobalVar.pas
@@ -67,7 +67,6 @@ end;
 initialization
   oAppName         := 'Fzam';
   oVersion         := '1.0.15.202605300100.alpha';
->>>>>>> a5d681139ed3b47e479aa2633865486365ec5065
   oUser            := 'No definido';
   oGroup           := 'No definido';
   oNomImpresoraCaja:='';
