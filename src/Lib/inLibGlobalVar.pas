78aeae46e7853606bc50912b902fbc09dadcf83d
Alejandro Laorden <alejandro.laorden@proton.me>
2026-06-15 22:34:56 +0200

    Mis cambios locales

--- src/Lib/inLibGlobalVar.pas
+++ src/Lib/inLibGlobalVar.pas
@@ -1,4 +1,4 @@
-{******************************************************************************}
+﻿{******************************************************************************}
 {                                                                              }
 {  Módulo:       inLibGlobalVar                                                }
 {    Tipo:       Librería                                                      }
@@ -71,7 +71,7 @@ end;
 
 initialization
   oAppName         := 'Fzam';
-  oVersion         := '1.0.15.202606150180.alpha';
+  oVersion         := '1.0.15.202606150190.alpha';
   oUser            := 'No definido';
   oGroup           := 'No definido';
   oNomImpresoraCaja:='';
